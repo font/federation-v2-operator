70b070651ae9d8f3d5f18cfb7272aaa29a3e7437
Maru Newby <marun@redhat.com>
2018-07-18 22:15:49 +0000

    INITIAL COMMIT

---
+++ pkg/apis/operator/v1alpha1/types.go
@@ -0,0 +1,29 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type FederationV2List struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []FederationV2 `json:"items"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type FederationV2 struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Spec              FederationV2Spec   `json:"spec"`
+	Status            FederationV2Status `json:"status,omitempty"`
+}
+
+type FederationV2Spec struct {
+	// Fill me
+}
+type FederationV2Status struct {
+	// Fill me
+}
